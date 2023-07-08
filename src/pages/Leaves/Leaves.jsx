2bd19d567f7cba37e5140ae38488b1055fe830cd
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2023-07-08 17:22:22 +0300

    leave page
    leave page

--- src/pages/Leaves/Leaves.jsx
+++ src/pages/Leaves/Leaves.jsx
@@ -39,11 +39,7 @@ export const Leaves = () => {
       ];
       
 
-    // const EmployeesData = employees.map((employee) => {
-    //     return(
-    //         <Employee key={employee.id} employee={employee}/>
-    //     );
-    // })
+
     return (
         <div className='leaves'>
             <TopSearch/>
@@ -58,59 +54,7 @@ export const Leaves = () => {
                     <Col></Col>
                     <Col></Col>
                 </Row>
-                {/* <div className='Table-leaves'>
-                        <Table responsive borderless>
-            <thead>
-                <tr>
-                {Array.from({ length: 1 }).map((id) => (
-                    <th className='Name-leaves' key={id}>id</th>
-                ))}
-                {Array.from({ length: 1 }).map((title) => (
-                    <th className='Name-leaves' key={title}>Name</th>
-                ))}
-                {Array.from({ length: 1 }).map((Department_Role) => (
-                    <th className='Department_Role-leaves' key={Department_Role}>Depatment Role</th>
-                ))}
-                {Array.from({ length: 1 }).map((Date) => (
-                    <th className='Date-leaves' key={Date}>Date</th>
-                ))}
-                {Array.from({ length: 1 }).map((Status) => (
-                    <th className='Status-leaves' key={Status}>Status</th>
-                ))}
-                {Array.from({ length: 1 }).map((Leave_From) => (
-                    <th className='Leave_From-leaves' key={Leave_From}>Leave From</th>
-                ))}
-                {Array.from({ length: 1 }).map((Leave_To) => (
-                    <th className='Leave_To-leaves' key={Leave_To}>Leave To</th>
-                ))}
-                {Array.from({ length: 1 }).map((Leave_Type) => (
-                    <th className='Leave_Type-leaves' key={Leave_Type}>Leave Type</th>
-                ))}
-                {Array.from({ length: 1 }).map((Reason) => (
-                    <th className='Reason-leaves' key={Reason}>Reason</th>
-                ))}
-                {Array.from({ length: 1 }).map((Action) => (
-                    <th className='Action-leaves' key={Action}>Action</th>
-                ))}
-                </tr> 
-            </thead>
-            <tbody>
 
-            {employees.map(employee => (
-              <>
-              <tr key={employee.id}>
-                <td>{employee.id}</td>
-                <td>{employee.title}</td>
-                <td>{employee.title}</td>
-                <td>{employee.price}</td>
-                <td>{employee.category}</td>
-                <td>{employee.category}</td>
-              </tr>
-              </>
-            ))}
-            </tbody>
-            </Table>
-            </div> */}
             <br/>
                     <TableContainer component={Paper}>
         <Table sx={{minWidth: 650 }} aria-label="simple table">
