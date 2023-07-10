6dac11f35b75427717f89941fe9162293a11e90a
mohammed tarek <102931301+m21102001@users.noreply.github.com>
2023-07-10 14:03:05 +0300

    Update Events.jsx

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,13 +1,12 @@
 import './events.css'
 import {TopSearch} from "../../components/TopSerarch/TopSearch.jsx";
-import Calender from '../../components/Clander/Calender';
 
 export const Events = () => {
     return (
         <div className='events'>
             <TopSearch/>
             Events
-            {/* <Calender/> */}
+          
         </div>
     )
 }
